2bd898a89c8d5b86dde5bc97f6258191065b4788
luisAlfonso <renumoo117@gmail.com>
2022-11-20 21:23:42 -0600

    añadiendo mejoras de query 4 y 6

--- Querys/6 Pruebas.sql
+++ Querys/6 Pruebas.sql
@@ -6,6 +6,10 @@ EXEC sp_GestionarOpcionDePago  @op='i', @Nombre ='Tarjeta de Credito';
 EXEC sp_GestionarOpcionDePago  @op='i', @Nombre ='Tarjeta de Debito';
 EXEC sp_GestionarOpcionDePago  @op='i', @Nombre ='Vale de Despensa';
 
+EXEC sp_GestionarUsuario @op='i',@IDUsuario	=0 ,@contraseña	='Marinas117', @nombres='Luis Alfonso', @apellidoPat='Martinez',@apellidoMat='Martinez',@CURP='MAML000123HNLRRSA7', @fechNac='2000-01-23', @numNomina='9364867774537734'		, @email='luis@mail.com'		, @esAdmin=1
+
+
+
 EXEC sp_GestionarUnidadDeMedida @op ='e',@IDUnidadMedida=10,@nombre='Kilo',@Descripcion ='Unidad de masa del sistema internacional.'
 EXEC sp_GestionarUnidadDeMedida @op ='e',@IDUnidadMedida=11,@nombre='Gramo',@Descripcion ='Unidad de masa, milésima parte de un kilogramo.'
 EXEC sp_GestionarUnidadDeMedida @op ='e',@IDUnidadMedida=12,@nombre='Miligramo',@Descripcion ='Unidad de masa, milesima parte de un gramo.'
@@ -28,7 +32,6 @@ EXEC sp_GestionarDescuento	 @op			= 'i'
 						,@FechaINI		= '2022-11-20'
 						,@FechaFIN		= '2022-12-1'
 						,@ProductoFK	= 1000000;
-
 EXEC sp_GestionarDescuento	 @op			= 'i'
 						,@IDDescuento	= 0
 						,@Nombre 		= 'pepsi_30'
@@ -36,7 +39,6 @@ EXEC sp_GestionarDescuento	 @op			= 'i'
 						,@FechaINI		= '2022-11-20'
 						,@FechaFIN		= '2022-12-1'
 						,@ProductoFK	= 1000001;
-
 EXEC sp_GestionarDescuento	 @op			= 'i'
 						,@IDDescuento	= 0
 						,@Nombre 		= 'Manzanas_30'
@@ -49,6 +51,8 @@ EXEC sp_GestionarDescuento	 @op			= 'i'
 						select * from Producto
 						Select* from Departamento
 						select* from Descuento
+
+						select*from Usuario
 
 						EXEC sp_GestionarProducto @op='s'
 						EXEc sp_GestionarTienda @op='s'
